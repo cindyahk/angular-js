e772dd88f92ead2f5020588345b9886000040949
cindyahkioon <cindyak86@gmail.com>
2017-12-10 11:35:26 +0100

    Udemy Assignment 2: Data Binding

---
+++ MySandbox/basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = "Cindy";
+
+  onResetUsername() {
+    this.username = "";
+  }
+}
